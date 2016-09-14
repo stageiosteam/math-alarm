943e8566f613428aa1abc24c97e873283392641a
Truong Cong Thanh <t_thanh@stagegroup.jp>
2016-09-14 15:59:37 +0700

    project moi tao - hue

--- AlarmIOS/ViewController.swift
+++ AlarmIOS/ViewController.swift
@@ -9,11 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+       
+          }
+    
+//    func createButton () {
+//        
+//             // button alarm
+//        let alarmBtn = UIButton(type: UIButtonType.Custom)
+//        alarmBtn.frame = CGRectMake(100, 100, 200, 100)
+//       // alarmBtn.setTitleColor(UIColor.blueColor(), forState: .Normal)
+//       // alarmBtn.setTitle("Test Button", forState: UIControlState.Normal)
+//        alarmBtn.setBackgroundImage(UIImage(named: "wazowskiAlarm.png"), forState: UIControlState.Normal)
+//        self.view.addSubview(alarmBtn)
+//        
+//        // button World Clock
+//        
+//        let worldColockBtn = UIButton(type: UIButtonType.Custom)
+//        worldColockBtn.frame = CGRectMake(100, 100, 200, 100)
+//        worldColockBtn.setBackgroundImage(UIImage(named: "wazowskiAlarm.png"), forState: UIControlState.Normal)
+//        self.view.addSubview(alarmBtn)
+//        
+//    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()

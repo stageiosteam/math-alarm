cfe25d58c3c31dcd4d3fe26e2dccb06eb4c6b82c
Truong Cong Thanh <t_thanh@stagegroup.jp>
2016-09-14 12:55:12 +0700

    Initial Commit

---
+++ AlarmIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlarmIOS
+//
+//  Created by sgv on 9/14/16.
+//  Copyright © 2016 sgv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
